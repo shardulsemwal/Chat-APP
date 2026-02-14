e0ac2e95c6872300d0d24e573acedc7ea8ed1a8c
Your Name <you@example.com>
2026-02-15 01:55:40 +0530

    Initial commit - Chat app ready for deployment

---
+++ nodeServer/index.js
@@ -0,0 +1,48 @@
+//node server which will handle socket io connections
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Serve static files from parent directory
+app.use(express.static(path.join(__dirname, '..')));
+
+// Serve index.html on root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+const users = {};
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+io.on('connection', socket=>{
+    console.log('New connection:', socket.id);
+    
+    socket.on('new-user-joined', name=>{
+        console.log(`${name} joined the chat`);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
